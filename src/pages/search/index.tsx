0e9c027279b6542e229635bce80b1da0811261db
danimkim <danimkim.dev@gmail.com>
2024-09-13 13:22:10 +0900

    feat: display movie list in search page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,12 +1,24 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
-
+import movies from "@/mock/movies.json";
+import MovieCard from "@/components/movie-card";
+import styles from "./index.module.css";
 export default function Search() {
   const router = useRouter();
   const { q } = router.query;
 
-  return <div>검색 결과: {q}</div>;
+  return (
+    <section className={styles.section}>
+      <h2>검색 결과: {`"${q}"`}</h2>
+      <div className={styles.searchResultList}>
+        {/* temp data */}
+        {movies.slice(0, 3).map((movie) => (
+          <MovieCard key={movie.id} {...movie} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 Search.getLayout = (page: ReactNode) => {
