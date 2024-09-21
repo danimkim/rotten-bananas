4b87ff0f525bdf854de517d2fef0eab1db6eb842
danimkim <danimkim.dev@gmail.com>
2024-09-22 02:57:51 +0900

    fix: make api request with https

--- src/lib/fetch-movie.ts
+++ src/lib/fetch-movie.ts
@@ -3,7 +3,7 @@ import { MovieData } from "@/types";
 export default async function fetchMovie(
   id: number
 ): Promise<MovieData | null> {
-  const url = `http://${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/movie/${id}`;
+  const url = `https://${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/movie/${id}`;
 
   try {
     const res = await fetch(url);
