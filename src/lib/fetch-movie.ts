e7911a28ce6181969cc519a0df9c8e1e83b2974a
danimkim <danimkim.dev@gmail.com>
2024-09-22 02:37:33 +0900

    refactor: modify api request url

--- src/lib/fetch-movie.ts
+++ src/lib/fetch-movie.ts
@@ -3,7 +3,7 @@ import { MovieData } from "@/types";
 export default async function fetchMovie(
   id: number
 ): Promise<MovieData | null> {
-  const url = `http://localhost:12345/movie/${id}`;
+  const url = `http://${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/movie/${id}`;
 
   try {
     const res = await fetch(url);
