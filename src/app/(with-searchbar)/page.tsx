5a70f10b08a7ede4ae5dea56ee1531a5bd8ebb2a
danimkim <danimkim.dev@gmail.com>
2024-09-25 00:41:27 +0900

    feat: style index page

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,3 +1,50 @@
+import { css, cva } from "../../../styled-system/css";
+import mockData from "./../mock/dummy.json";
+import MovieCard from "./components/MovieCard";
+
 export default function Home() {
-  return <div>인덱스 페이지</div>;
+  return (
+    <>
+      <section>
+        <h2 className={headingStyle}>지금 가장 추천하는 영화</h2>
+        <div className={containerRecipe({ column: "3" })}>
+          {mockData.slice(0, 3).map((data) => (
+            <MovieCard key={data.id} {...data} />
+          ))}
+        </div>
+      </section>
+      <section>
+        <h2 className={headingStyle}>등록된 모든 영화</h2>
+        <div className={containerRecipe({ column: "5" })}>
+          {mockData.map((data) => (
+            <MovieCard key={data.id} {...data} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
+
+const headingStyle = css({
+  marginTop: "10",
+  fontSize: "20px",
+  fontWeight: 600,
+});
+
+const containerRecipe = cva({
+  base: {
+    marginTop: "5",
+    display: "grid",
+    gap: "2",
+  },
+  variants: {
+    column: {
+      "3": {
+        "grid-template-columns": "repeat(3, 1fr)",
+      },
+      "5": {
+        "grid-template-columns": "repeat(5, 1fr)",
+      },
+    },
+  },
+});
