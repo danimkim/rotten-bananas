94ace53f533e92991808b5e337ed76e8d3defb9b
danimkim <danimkim.dev@gmail.com>
2024-09-26 22:23:19 +0900

    refactor: use data from API

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "media.themoviedb.org" }],
   },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production" && {
+      exclude: ["error"],
+    },
+  },
 };
 
 export default nextConfig;
